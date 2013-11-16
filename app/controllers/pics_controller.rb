5a391053ab4d38cc6b8eddb22a7e7f0eec9cc69d
Linda Leung <lindaleung@Tonys-MacBook-Pro.local>
2013-11-16 17:23:42 -0500

    Added new and create to the pics controller

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -6,4 +6,24 @@ class PicsController < ApplicationController
 
 	end
 
+	def new
+
+		@pic = Pic.new
+	
+	end
+
+	def create
+		@pic = Pic.create( pic_params )
+		redirect_to pics_path
+	end
+
+
+	private
+
+	# get stuff out of the form
+	def pic_params
+		# what you want to allow the user to change
+		params.require(:pic).permit(:lesson, :emotion, :learned)
+	end
+
 end
